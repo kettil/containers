ed3e5503d6403b9d26d7ad16883f9a604ac59de5
Kjell Dießel <development@kettil.de>
2021-12-26 00:01:28 +0100

    feat: 🐣

---
+++ webdav/entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -e
+
+HTTPD_PREFIX="${HTTPD_PREFIX:-/usr/local/apache2}"
+
+if [ "$AUTH_TYPE" != "" ]; then
+    # Only support "Basic" and "Digest".
+    if [ "$AUTH_TYPE" != "Basic" ] && [ "$AUTH_TYPE" != "Digest" ]; then
+        printf '%s\n' "$AUTH_TYPE: Unknown AuthType" 1>&2
+        exit 1
+    fi
+    sed -e "s|AuthType .*|AuthType $AUTH_TYPE|" -i "$HTTPD_PREFIX/conf/conf-available/dav.conf"
+fi
+
+if [ ! -e "/user.passwd" ]; then
+    touch "/user.passwd"
+    if [ "$USERNAME" != "" ] && [ "$PASSWORD" != "" ]; then
+        if [ "$AUTH_TYPE" = "Digest" ]; then
+            HASH="$(printf '%s' "$USERNAME:WebDAV:$PASSWORD" | md5sum | awk '{print $1}')"
+            printf '%s\n' "$USERNAME:WebDAV:$HASH" > /user.passwd
+        else
+            htpasswd -B -b -c "/user.passwd" $USERNAME $PASSWORD
+        fi
+    fi
+fi
+
+[ ! -d "/webdav/data" ] && mkdir -p "/webdav/data"
+[ ! -e "/webdav/DavLock" ] && touch "/webdav/DavLock"
+chown -R www-data:www-data "/webdav"
+
+exec "$@"
