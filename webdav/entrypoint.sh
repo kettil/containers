cf243fbf9991b818811182d9eeec63791f66c72d
Kjell Dießel <616311+kettil@users.noreply.github.com>
2022-08-08 18:39:10 +0100

    fix(webdav): fix the problem with the temp file

--- webdav/entrypoint.sh
+++ webdav/entrypoint.sh
@@ -4,12 +4,7 @@ set -e
 HTTPD_PREFIX="${HTTPD_PREFIX:-/usr/local/apache2}"
 
 if [ "$USERNAME" != "" ] && [ "$PASSWORD" != "" ]; then
-    if [ "$AUTH_TYPE" = "Digest" ]; then
-        HASH="$(printf '%s' "$USERNAME:WebDAV:$PASSWORD" | md5sum | awk '{print $1}')"
-        printf '%s\n' "$USERNAME:WebDAV:$HASH" > /webdav/config/user.passwd
-    else
-        htpasswd -B -b -c "/webdav/config/user.passwd" $USERNAME $PASSWORD
-    fi
+    htpasswd -B -n -b $USERNAME $PASSWORD > "/webdav/config/user.passwd"
 fi
 
 exec "$@"
